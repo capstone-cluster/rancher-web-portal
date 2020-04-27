902ee3705829efe5ca98b50afe5452234b2e3405
T Fleck <emailtfleck@gmail.com>
2020-04-27 16:29:27 -0400

    move list of images server-side

--- app.js
+++ app.js
@@ -10,10 +10,17 @@ const host = "localhost";
 const key_path = "./certs/encrypted.key";
 const cert_path = "./certs/server.cert";
 
+const images = ['Default - VS Code', 'Dr. Novocin - VS Code', 'Dr. Cotton - Nessus', 'Dr. Cotton - OpenVAS'];
+
 app.use("/static", express.static(path.join(__dirname, "public_html")));
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/public_html/index.html");
+});
+
+// Get definintion of the provided word from the dictionary
+app.get("/api/images", function(request, response) {
+  response.send(images);
 });
 
 // Configure an HTTPS server
--- public_html/main.js
+++ public_html/main.js
@@ -22,4 +22,41 @@ $(document).ready(function () {
       $($this.parents('form').find('.form-submit-btn')).prop('disabled', !formIsValid);
     }
   });
+
+  $('#inputForm').submit(function (e) {
+    e.preventDefault();
+    if ($($('#inputForm').find('.form-submit-btn')).hasClass('disabled')) {
+      e.stopPropagation();
+      return;
+    }
+
+    let input_image = $('#input_image').val();
+    let input_email = $('#input_email').val();
+    let input_pass = $('#input_pass').val();
+
+    console.log(input_image);
+    console.log(input_email);
+    console.log(input_pass);
+
+    // reset form
+    $('#input_email').removeClass('is-valid');
+    $('#input_pass').removeClass('is-valid');
+    $('#inputForm').trigger('reset');
+    $($('#inputForm').find('.form-submit-btn')).prop('disabled', true);
+  });
+
+  getImages(); // get list of deployable containers from server
 });
+
+function getImages(){
+  $.ajax({ 
+    url: '/api/images', 
+    type: 'GET', 
+    contentType: 'application/json' 
+  }).done(function (data) { 
+    console.log(data);
+    for(i=0; i<data.length; i++){
+      $('#input_image').append(`<option value="${i}">${data[i]}</option>`);
+    }
+  });
+}
